a5240abc34c10273e8c52916ae424cbde1cf62fc
Carson Brown <carsonbrown47@gmail.com>
2023-02-28 13:53:05 -0500

    removed console log

--- api/app.js
+++ api/app.js
@@ -22,7 +22,6 @@ app.use((error, req, res, next) => {
 	const status = error.statusCode || 500;
 	const message = error.message;
 	const data = error.data;
-	console.log(error);
 	res.status(status).json({ message: message, data: data });
 });
 
