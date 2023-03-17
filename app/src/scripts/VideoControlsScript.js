34f656e0cb1e6f6399637b19aae6f228f1cac5ff
Drew Cross <drew.cross19@gmail.com>
2023-03-16 22:08:02 -0400

    chore: removed un-needed js file

--- app/src/scripts/VideoControlsScript.js
+++
@@ -1,4 +0,0 @@
-const videoPlayer = document.querySelector("#video-player");
-const controls = document.querySelector(".controls--container");
-controls.style.height = videoPlayer.offsetHeight + "px";
-controls.style.width = videoPlayer.offsetWidth + "px";
