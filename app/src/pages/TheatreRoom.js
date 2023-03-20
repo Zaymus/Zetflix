d2546efb16c11ab8f0cc4635fbbef6eae7fbe45e
Carson Brown <carsonbrown47@gmail.com>
2023-03-20 19:55:52 -0400

    chore: converted theatre room function component to class component

--- app/src/pages/TheatreRoom.js
+++ app/src/pages/TheatreRoom.js
@@ -1,28 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import VideoPlayer from '../components/video/VideoPlayer';
 import Chat from '../components/chat/Chat';
 import NotificationList from '../components/ui/Notification/NotificationList';
 import openSocket from 'socket.io-client';
 import './TheatreRoom.css';
 
-const TheatreRoom = (props) => {
+class TheatreRoom extends Component {
+  state = {
+    socket: null,
+    captions: "",
+  }
 
-  const [socket, setSocket] = useState(null);
+  componentDidMount() {
+    const socket = openSocket("http://localhost:9000", {transports:["websocket"]});
+    socket.emit('room.join', {roomId: this.props.roomId, userId: "64066123269b0611c1872182"});
+    this.setState({socket: socket});
 
-  useEffect(() => {
-    if(socket === null) {
-      setSocket(openSocket("http://localhost:9000", {transports:["websocket"]}));
-    }
-
-    if(socket) {
-      socket.emit('room.join', {roomId: props.roomId, userId: "64066123269b0611c1872182"});
-    }
-  }, [socket, props.roomId]);
-  
-  const [captions, setCaptions] = useState("");
-
-  if(!captions) {
-    fetch(`http://localhost:9000/api/caption/${props.videoId}`, {
+    fetch(`http://localhost:9000/api/caption/${this.props.videoId}`, {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
@@ -32,20 +26,22 @@ const TheatreRoom = (props) => {
       return response.json();
     })
     .then(jsonData => {
-      setCaptions(jsonData);
+      this.setState({captions: jsonData});
     })
     .catch(err => {
       console.log(err);
     });
   }
 
-  return (
-    <div className='room--container'>
-      <VideoPlayer videoId={props.videoId} captions={captions} room={props.roomId} socket={socket} />
-      <Chat roomId={props.roomId} socket={socket}/>
-      <NotificationList socket={socket}/>
-    </div>
-  );
+  render() {
+    return (
+      <div className='room--container'>
+        <VideoPlayer videoId={this.props.videoId} captions={this.state.captions} room={this.props.roomId} socket={this.state.socket} />
+        <Chat roomId={this.props.roomId} socket={this.state.socket}/>
+        <NotificationList socket={this.state.socket}/>
+      </div>
+    );
+  }
 }
 
 export default TheatreRoom;
