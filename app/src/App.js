199812d2643aa84042d67f66a70c10d4db23a774
Carson Brown <carsonbrown47@gmail.com>
2023-03-20 19:59:13 -0400

    chore: converted App function component to class component

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
-// import Video from "./pages/Video";
+import React, { Component } from 'react';
+import Video from "./pages/Video";
 import TheatreRoom from "./pages/TheatreRoom";
 
-const App = () => {
-	// return (<Video videoId="63fd57ad00f5e1f9186f4daf"/>);
-	return (<TheatreRoom videoId="63fd57ad00f5e1f9186f4daf" roomId="64077e3cb7676b8118f58739"/>);
-	// return (<div></div>)
-};
+class App extends Component {
+	state = {
+		token: null,
+		userId: null,
+	}
+
+	loginHandler(event, authData) {
+		event.preventDefault();
+		fetch('http://localhost:9000/api/auth/login', {
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				username: authData.username,
+				password: authData.password,
+			}),
+		})
+		.then((res) => {
+			if(res.status === 200) {
+				return res.json();
+			}
+		})
+		.then((resData) => {
+			this.setState({
+				token: resData.token,
+				userId: resData.userId,
+			});
+			localStorage.setItem('token', resData.token);
+			localStorage.setItem('userId', resData.userId);
+		})
+		.catch((err) => {
+			console.log(err);
+		})
+	}
+
+	render() {
+		// return (<Video videoId="63fd57ad00f5e1f9186f4daf"/>);
+		return (<TheatreRoom videoId="63fd57ad00f5e1f9186f4daf" roomId="64077e3cb7676b8118f58739"/>);
+		// return (<div></div>)
+	}
+}
 
 export default App;
