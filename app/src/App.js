3dd215db4626828b985584e311bcb2ae807c2299
Carson Brown <carsonbrown47@gmail.com>
2023-03-27 18:59:34 -0400

    chore: reverted back to functional component to allow for redirecting back to pages when login was mandatory

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
-import {Route, Routes, Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Route, Routes, Link, useLocation, useNavigate} from 'react-router-dom';
 import Video from "./pages/Video";
 import TheatreRoom from "./pages/TheatreRoom";
 import Login from './pages/Login';
 
-class App extends Component {
-	state = {
-		token: null,
-		userId: null,
-	}
+const App = () => {
 
-	loginHandler(event, authData) {
+	const [state, setState] = useState({token: null, userId: null});
+	const location = useLocation();
+	const navigate = useNavigate();
+
+	const loginHandler = (event, authData) => {
 		event.preventDefault();
+		
+		var redirectUrl = location.search.split("=");
+		redirectUrl = redirectUrl[1] || "/";
+		
 		fetch('http://localhost:9000/api/auth/login', {
 			method: "POST",
 			headers: {
@@ -28,34 +32,33 @@ class App extends Component {
 			}
 		})
 		.then((resData) => {
-			this.setState({
+			setState({
 				token: resData.token,
 				userId: resData.userId,
 			});
 			localStorage.setItem('token', resData.token);
 			localStorage.setItem('userId', resData.userId);
 			localStorage.setItem('username', resData.username);
+			navigate(redirectUrl);
 		})
 		.catch((err) => {
 			console.log(err);
 		})
 	}
 
-	render() {
-		return(
-			<div>
-				<Link to="/login">Login</Link>
-				<Link to="/video/63fd57ad00f5e1f9186f4daf">Watch Video</Link>
-				{ this.state.token && <Link to="/theatre-room/64077e3cb7676b8118f58739/video/63fd57ad00f5e1f9186f4daf">Theatre Room</Link> }
+	return (
+		<div>
+			<Link to="/login">Login</Link>
+			<Link to="/video/63fe4be525aafdeef1686567">Watch Video</Link>
+			{ state.token && <Link to="/theatre-room/64077e3cb7676b8118f58739/video/63fd57ad00f5e1f9186f4daf">Theatre Room</Link> }
 
-				<Routes>
-					<Route path="/login" element={<Login onLogin={this.loginHandler.bind(this)} />} />
-					<Route path="/video/:videoId" element={<Video />}/>
-					{ this.state.token && <Route path="/theatre-room/:roomId/video/:videoId" element={<TheatreRoom />}/> }
-				</Routes>
-			</div>
-		)
-	}
+			<Routes>
+				<Route path="/login" element={<Login onLogin={loginHandler.bind(this)} />} />
+				<Route path="/video/:videoId" element={<Video />}/>
+				{ state.token && <Route path="/theatre-room/:roomId/video/:videoId" element={<TheatreRoom />}/> }
+			</Routes>
+		</div>
+	)
 }
 
 export default App;
